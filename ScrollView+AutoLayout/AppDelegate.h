fe932346e50c0289793417341dc3a31fb65fc4f5
Milan Panchal <sam07it22@gmail.com>
2015-01-21 22:31:36 +0530

    Initial Commit

---
+++ ScrollView+AutoLayout/AppDelegate.h
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.h
+//  ScrollView+AutoLayout
+//
+//  Created by MilanPanchal on 21/01/15.
+//  Copyright (c) 2015 Pantech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <CoreData/CoreData.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (readonly, strong, nonatomic) NSManagedObjectContext *managedObjectContext;
+@property (readonly, strong, nonatomic) NSManagedObjectModel *managedObjectModel;
+@property (readonly, strong, nonatomic) NSPersistentStoreCoordinator *persistentStoreCoordinator;
+
+- (void)saveContext;
+- (NSURL *)applicationDocumentsDirectory;
+
+
+@end
+
---
+++ ScrollView+AutoLayout/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ScrollView+AutoLayout
+//
+//  Created by MilanPanchal on 21/01/15.
+//  Copyright (c) 2015 Pantech. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
