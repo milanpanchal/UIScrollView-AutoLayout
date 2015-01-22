ffdf46d1c6773d460d604c3bbcf3de3619644277
Milan Panchal <sam07it22@gmail.com>
2015-01-22 19:01:38 +0530

    Demo Project first version

---
+++ README.md
@@ -0,0 +1,21 @@
+#UIScrollView+AutoLayout
+
+
+This sample project show how to setup a UIScrollView with Auto Layout. Also how to handle UIScrollView when keyboard will appear. 
+
+
+
+
+##Contact:
+
+
+Follow me on 
+
+1. **Twitter** ([@milan_panchal24](https://twitter.com/milan_panchal24))
+
+2. **Github** ([/milanpanchal](https://github.com/milanpanchal/))
+
+
+##License:
+
+This Project is available under the MIT license. See the [LICENSE](https://github.com/milanpanchal/UIScrollView-AutoLayout/blob/master/LICENSE) file for more info.
--- ScrollView+AutoLayout/ViewController.m
+++ ScrollView+AutoLayout/ViewController.m
@@ -8,20 +8,122 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UITextFieldDelegate>
+
+@property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (weak, nonatomic) IBOutlet UIView *contentView;
+
+@property (weak, nonatomic) IBOutlet UITextField *firstNameTF;
+@property (weak, nonatomic) IBOutlet UITextField *lastNameTF;
+@property (weak, nonatomic) IBOutlet UITextField *phoneNumberTF;
+@property (weak, nonatomic) IBOutlet UITextField *emailAddressTF;
+@property (weak, nonatomic) IBOutlet UITextField *bloodGroupTF;
+
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *constraintBottomSpace;
+
+@property (weak, nonatomic) UITextField *activeField;
 
 @end
+
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardDidShow:)
+                                                 name:UIKeyboardDidShowNotification
+                                               object:nil];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self
+                                             selector:@selector(keyboardWillBeHidden:)
+                                                 name:UIKeyboardWillHideNotification
+                                               object:nil];
+
+
+}
+
+-(void)viewDidAppear:(BOOL)animated {
+
+    self.scrollView.contentSize = self.contentView.frame.size;
+
+    NSLog(@"Content Size %@",NSStringFromCGSize(self.scrollView.contentSize));
+}
+
+
+- (void)viewWillDisappear:(BOOL)animated {
+
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardDidShowNotification
+                                                  object:nil];
+    
+    [[NSNotificationCenter defaultCenter] removeObserver:self
+                                                    name:UIKeyboardWillHideNotification
+                                                  object:nil];
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - UITextFieldDelegate
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+
+    
+    if (self.firstNameTF == textField) {
+        [self.lastNameTF becomeFirstResponder];
+    
+    } else if (self.lastNameTF == textField) {
+        [self.emailAddressTF becomeFirstResponder];
+    
+    } else if (self.emailAddressTF == textField) {
+        [self.phoneNumberTF becomeFirstResponder];
+    
+    } else if (self.phoneNumberTF == textField) {
+        [self.bloodGroupTF becomeFirstResponder];
+    
+    } else if (self.bloodGroupTF == textField) {
+        [self.bloodGroupTF resignFirstResponder];
+    }
+
+    return YES;
+}
+
+
+- (void)textFieldDidBeginEditing:(UITextField *)sender {
+    self.activeField = sender;
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)sender {
+    self.activeField = nil;
+}
+
+
+#pragma mark - NSNotificationCenter for Keyboard
+
+- (void) keyboardDidShow:(NSNotification *)notification {
+
+    // Get keyboard frame
+    CGRect frame = [notification.userInfo[UIKeyboardFrameEndUserInfoKey] CGRectValue];
+
+    CGRect aRect = self.view.frame;
+    aRect.size.height -= frame.size.height;
+    if (!CGRectContainsPoint(aRect, self.activeField.frame.origin) ) {
+        [self.scrollView scrollRectToVisible:self.activeField.frame animated:YES];
+    }
+    
+    self.constraintBottomSpace.constant = frame.size.height;
+    [self.view layoutIfNeeded];
+
+}
+
+- (void) keyboardWillBeHidden:(NSNotification *)notification {
+    self.constraintBottomSpace.constant = 0;
+    [self.view layoutIfNeeded];
 }
 
 @end
